04d3fdbdf33599567cea9bdba1957beb16b982c1
Hovenbet <91114408+Hovenbet@users.noreply.github.com>
2023-03-15 08:23:59 +0100

    hello

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,36 +3,73 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement = new Equipement[2];
+	private int nbEquipement=0;
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		
+		assert force > 0 : "FORCE EST TOUJOURS POSITIVE";
 	}
+	
+	private void sEquiper(Equipement equi) {
+		switch(nbEquipement) {
+		case 2:
+			System.out.println("Le soldat "+ this.nom+" est déjà bien protégé !");
+		
+		case 1:
+			if(equipement[nbEquipement]==equi)
+			System.out.println("Le soldat "+ this.nom+" possède déjà un "+ equipement[1]+" !");
+		
+		default:
+		
+			equipement[nbEquipement]=equi;
+			nbEquipement++;
+			System.out.println("Le soldat " +this.nom+" s’équipe avec un " + equi+".");
+		}
+	}
+	
+	
+	
 	public String getNom() {
 		return nom;
 	}
+	
+	
 public void parler(String texte) {
 	System.out.println(prendreParole()+ "<<" + texte + ">>");
 
 }
+
+
 private String prendreParole() {
 	return "Le romain " + nom + " : ";
 	
 }
 
+
 public void recevoirCoup(int forceCoup) {
+	assert force > 0 : "FORCE D'UN ROMAIN EST TOUJOURS POSITIVE";
 	force-=forceCoup;
 	if (force>0) {
 		parler("Aïe");
 	} else {
 		parler("J'abondonne...");
 	}
-
+	assert force > 0 : "FORCE D'UN ROMAIN EST TOUJOURS POSITIVE";
 }
+
+
 public static void main(String[] args) {
-	Romain romi= new Romain("Romi",10);
-	romi.prendreParole();
-	romi.parler("Hello this is me");
-	romi.recevoirCoup(2);
+	Romain Minus= new Romain("Minus",6);
+//	Minus.prendreParole();
+//	Minus.parler("Hello this is me");
+//	Minus.recevoirCoup(2);
+
+Minus.sEquiper(Equipement.CASQUE);
+Minus.sEquiper(Equipement.CASQUE);
+Minus.sEquiper(Equipement.BOUCLIER);
+Minus.sEquiper(Equipement.CASQUE);
 
 }
 
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -2,11 +2,65 @@ package personnages;
 
 public class Village {
 	private String nom;
+	private Chef chef;
+	private int nbVillageois=0;
+	private int nbVillageoisMaximum;
+	
+	private Gaulois[] villageois;
+	
+	public Village(String nom,int nbVillageoisMaximum) {
+		this.nbVillageoisMaximum=nbVillageoisMaximum;
+		this.nom = nom;
+		this.villageois= new Gaulois[nbVillageoisMaximum];
+	}
+	
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
+
 	public String getNom() {
 		return nom;
 	}
-	public Village() {
+	
+
+	private Gaulois trouverHabitant(int numVillgeois) {
+		return villageois[numVillgeois];
+	}
+	
+	public void ajouterHabitant(Gaulois gaulois) {
+		if(nbVillageois<nbVillageoisMaximum) {
+			villageois[nbVillageois]=gaulois;
+			nbVillageois++;
+		}
+	}
+	public void afficherVillageois() {
+		System.out.println("Dans village du chef "+ chef.getNom() + " vivent les légendaires gaulois");
+		for(int i=0; i<nbVillageois;i++) {
+			Gaulois gaulois=villageois[i];
+			System.out.println("-"+gaulois.getNom());
+		}
+	}
+	public static void main(String[] args) {
+		Village village= new Village("Village des Irréductibles",30);
+		//Gaulois gaulois = village.trouverHabitant(30);		
+		/*Exception in thread "main" java.lang.NullPointerException: Cannot load from object array because "this.villageois" is null
+			at LesGaulois/personnages.Village.trouverHabitant(Village.java:27)
+			at LesGaulois/personnages.Village.main(Village.java:33)
+			
+			on obtient cette exception car il y a aucun habitant eet l'habitant 30 n'existe pas*/
+		
+		Chef chef= new Chef("Abraracourcix",6,village);
+		Gaulois Asterix= new Gaulois("Asterix",8);
+		village.setChef(chef);
+		village.ajouterHabitant(Asterix);
+		Gaulois Obelix= new Gaulois("Obélix",6);
+		village.ajouterHabitant(Obelix);
+
+//		Gaulois gaulois = village.trouverHabitant(0);
+//		System.out.println(gaulois);
+		
+		village.afficherVillageois();
 		
 	}
-
+	
 }
